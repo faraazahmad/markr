bbae7718c582f6002b750bfd0bbcd8e3ae0d5dc9
Syed Faraaz Ahmad <faraaz98@live.com>
2018-05-31 21:45:17 +0530

    update bug remains

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', function() {
-    new Vue({
-        el: '#editor',
-        data: {
-          input: '# hello'
-        },
-        computed: {
-          compiledMarkdown: function () {
-            return marked(this.input, { sanitize: true })
-          }
-        },
-        methods: {
-          update: function (e) {
-            this.input = e.target.value
-          }
-        }
-    })
+  new Vue({
+    el: '#editor',
+    data: {
+      input: '# hello'
+    },
+    computed: {
+      compiledMarkdown: function () {
+        return marked(this.input, { sanitize: true })
+      }
+    },
+    methods: {
+      update: function (e) {
+        this.input = e.target.value
+      }
+    }
+  });
 });
