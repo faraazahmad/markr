55bcae03b0b4eff4aca9fde972a469e033630391
Syed Faraaz Ahmad <faraaz98@live.com>
2018-06-02 22:06:22 +0530

    add dark theme

--- js/app.js
+++ js/app.js
@@ -12,3 +12,16 @@ window.addEventListener('load', function() {
     }
   });
 });
+
+function changeTheme(themeNumber) {
+  // get css theme tag
+  let themeTag = document.querySelectorAll('link')[2];
+  if (themeNumber == 0) {
+    // change to dark
+    themeTag.setAttribute('href', 'css/dark.css');
+  }
+  else if (themeNumber == 1) {
+    // change to light
+    themeTag.setAttribute('href', 'css/light.css');
+  }
+}
